44f4f751ce44b2c5f8b58241dd1272829090e9c5
Annie Chen <a.janice.chen@gmail.com>
2024-05-02 13:53:17 +0200

    add: more carousel items

--- app/components/UspCarousel.tsx
+++ app/components/UspCarousel.tsx
@@ -10,42 +10,74 @@ const giftIcon = <FontAwesomeIcon icon={faGift} />;
 const icons = [truckIcon, returnIcon, giftIcon]
 const usps = ['Gratis thuisbezorgd vanaf €50,-', 'Gratis retourneren binnen 30 dagen', 'Gratis cadeauverpakking']
 
-const uspsObj = [ 
+const uspsObj = [
   {
-      id: 1,
-      name: 'Gratis thuisbezorgd vanaf €50,-',
-      icon: {truckIcon}
-    },
+    id: 1,
+    name: 'Gratis thuisbezorgd vanaf €50,-',
+    icon: { truckIcon }
+  },
   {
-      id: 2,
-      name: 'Gratis retourneren binnen 30 dagen',
-      icon: {returnIcon}
+    id: 2,
+    name: 'Gratis retourneren binnen 30 dagen',
+    icon: { returnIcon }
   },
   {
     id: 3,
     name: 'Gratis cadeauverpakking',
-    icon: {giftIcon}
-},
+    icon: { giftIcon }
+  },
 ]
 
 export function UspCarousel() {
-  return(
-    <div className="carousel-container">
-      <div className='carousel-wrapper'>
+  return (
+    <div>
+      <div className="carousel-container">
         {/* {icons.map((icon) => ( <div className='usp-item' key={icon.toString()}>{icon}</div>))} */}
         {/* {usps.map((usp) => ( <div className='usp-item' key={usp}>{usp}</div>))} */}
-        <div className='carousel-item'>
-          <FontAwesomeIcon icon={faTruck} />
-          <p>Gratis thuisbezorgd vanaf €50,-</p>
-        </div>
-        <div className='carousel-item'>
-          <FontAwesomeIcon icon={faRotateLeft} />
-          <p>Gratis retourneren binnen 30 dagen</p>
-        </div>
-        <div className='carousel-item'>
-          <FontAwesomeIcon icon={faGift} />
-          <p>Gratis cadeauverpakking</p>
-        </div>
+        <ul className='carousel-wrapper'>
+          <li className='carousel-item'>
+            <FontAwesomeIcon icon={faTruck} />
+            Gratis thuisbezorgd vanaf €50,-
+          </li>
+          <li className='carousel-item'>
+            <FontAwesomeIcon icon={faRotateLeft} />
+            Gratis retourneren binnen 30 dagen
+          </li>
+          <li className='carousel-item'>
+            <FontAwesomeIcon icon={faGift} />
+            Gratis cadeauverpakking
+          </li>
+        </ul>
+        {/* second node */}
+        <ul className='carousel-wrapper'>
+          <li className='carousel-item'>
+            <FontAwesomeIcon icon={faTruck} />
+            Gratis thuisbezorgd vanaf €50,-
+          </li>
+          <li className='carousel-item'>
+            <FontAwesomeIcon icon={faRotateLeft} />
+            Gratis retourneren binnen 30 dagen
+          </li>
+          <li className='carousel-item'>
+            <FontAwesomeIcon icon={faGift} />
+            Gratis cadeauverpakking
+          </li>
+        </ul>
+        {/* third node */}
+        <ul className='carousel-wrapper'>
+          <li className='carousel-item'>
+            <FontAwesomeIcon icon={faTruck} />
+            Gratis thuisbezorgd vanaf €50,-
+          </li>
+          <li className='carousel-item'>
+            <FontAwesomeIcon icon={faRotateLeft} />
+            Gratis retourneren binnen 30 dagen
+          </li>
+          <li className='carousel-item'>
+            <FontAwesomeIcon icon={faGift} />
+            Gratis cadeauverpakking
+          </li>
+        </ul>
       </div>
     </div>
   )
