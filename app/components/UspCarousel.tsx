b7346df5b92f2927ed7058ee17e9247b41d183ec
Annie Chen <a.janice.chen@gmail.com>
2024-05-13 11:31:04 +0200

    add: carousel slide animation

--- app/components/UspCarousel.tsx
+++ app/components/UspCarousel.tsx
@@ -3,37 +3,13 @@ import { faTruck } from "@fortawesome/free-solid-svg-icons";
 import { faRotateLeft } from "@fortawesome/free-solid-svg-icons";
 import { faGift } from "@fortawesome/free-solid-svg-icons";
 
-const truckIcon = <FontAwesomeIcon icon={faTruck} />;
-const returnIcon = <FontAwesomeIcon icon={faRotateLeft} />;
-const giftIcon = <FontAwesomeIcon icon={faGift} />;
-
-const icons = [truckIcon, returnIcon, giftIcon]
-const usps = ['Gratis thuisbezorgd vanaf €50,-', 'Gratis retourneren binnen 30 dagen', 'Gratis cadeauverpakking']
-
-const uspsObj = [
-  {
-    id: 1,
-    name: 'Gratis thuisbezorgd vanaf €50,-',
-    icon: { truckIcon }
-  },
-  {
-    id: 2,
-    name: 'Gratis retourneren binnen 30 dagen',
-    icon: { returnIcon }
-  },
-  {
-    id: 3,
-    name: 'Gratis cadeauverpakking',
-    icon: { giftIcon }
-  },
-]
+// const uspContent = document.querySelector(".carousel-wrapper")!.cloneNode(true);
+// document.querySelector(".carousel-container")!.appendChild(uspContent);
 
 export function UspCarousel() {
   return (
-    <div>
+    <>
       <div className="carousel-container">
-        {/* {icons.map((icon) => ( <div className='usp-item' key={icon.toString()}>{icon}</div>))} */}
-        {/* {usps.map((usp) => ( <div className='usp-item' key={usp}>{usp}</div>))} */}
         <ul className='carousel-wrapper'>
           <li className='carousel-item'>
             <FontAwesomeIcon icon={faTruck} />
@@ -48,7 +24,7 @@ export function UspCarousel() {
             Gratis cadeauverpakking
           </li>
         </ul>
-        {/* second node */}
+
         <ul className='carousel-wrapper'>
           <li className='carousel-item'>
             <FontAwesomeIcon icon={faTruck} />
@@ -63,7 +39,7 @@ export function UspCarousel() {
             Gratis cadeauverpakking
           </li>
         </ul>
-        {/* third node */}
+
         <ul className='carousel-wrapper'>
           <li className='carousel-item'>
             <FontAwesomeIcon icon={faTruck} />
@@ -78,7 +54,23 @@ export function UspCarousel() {
             Gratis cadeauverpakking
           </li>
         </ul>
+
+        <ul className='carousel-wrapper'>
+          <li className='carousel-item'>
+            <FontAwesomeIcon icon={faTruck} />
+            Gratis thuisbezorgd vanaf €50,-
+          </li>
+          <li className='carousel-item'>
+            <FontAwesomeIcon icon={faRotateLeft} />
+            Gratis retourneren binnen 30 dagen
+          </li>
+          <li className='carousel-item'>
+            <FontAwesomeIcon icon={faGift} />
+            Gratis cadeauverpakking
+          </li>
+        </ul>
+
       </div>
-    </div>
+    </>
   )
 }
